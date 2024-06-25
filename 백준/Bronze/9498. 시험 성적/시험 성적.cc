2c76de569709fe3c584b9b53f058c87fd16d92dd
이학민 (HAKMIN) <132976976+hakmin1015@users.noreply.github.com>
2024-06-25 17:33:44 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    scanf("%d", &x);
+    
+    if(x>=90)
+        printf("A");
+    else if(x>=80)
+        printf("B");
+    else if(x>=70)
+        printf("C");
+    else if(x>=60)
+        printf("D");
+    else
+        printf("F");
+    
+    return 0;
+}

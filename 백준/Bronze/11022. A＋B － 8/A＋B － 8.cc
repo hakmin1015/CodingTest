725bc60e25b1d52d060068e5e572214447db5889
이학민 (HAKMIN) <132976976+hakmin1015@users.noreply.github.com>
2024-06-25 17:37:05 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a,b,T;
+    scanf("%d", &T);
+    
+    for(int i=0; i<T; i++)
+    {
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d + %d = %d\n", i+1,a,b,a+b);
+    }
+    
+    return 0;
+}
